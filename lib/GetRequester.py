f682c7e649897177d2b002de68c4534d29631552
Sitoh7 <ha.wakhule@gmail.com>
2025-01-16 18:31:20 +0300

    Done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,6 +8,11 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+        response = requests.get(self.url)
+        
+        return response.content
 
     def load_json(self):
         pass
+        json_response = requests.get(self.url).json()
+        return json_response
